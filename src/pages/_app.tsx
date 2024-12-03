424bae4cdc60840f4174bcedd07c873be4cf9d4a
AsadKirmani <32667125+AsadKirmani@users.noreply.github.com>
2024-12-03 16:39:19 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,13 @@ import '../styles/globals.css';
 
 import axios from 'axios';
 import type { AppProps } from 'next/app';
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	  const [isHashValid, setIsHashValid] = useState(false);
-	    const [username, setUsername] = useState<string | null>(null);
+	  //const [isHashValid, setIsHashValid] = useState(false);
+	    //const [username, setUsername] = useState<string | null>(null);
 
-	      useEffect(() => {
+	      /*useEffect(() => {
 		          if (typeof window !== 'undefined') {
 				        const initData = window.Telegram.WebApp.initData;
 
@@ -26,9 +26,9 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 																	        if (!isHashValid) {
 																			    return null;
-																			      }
+																			      }*/
 
-																			        return <Component {...pageProps} username={username} />;
+return <Component {...pageProps} />;
 }
 
 export default MyApp;
