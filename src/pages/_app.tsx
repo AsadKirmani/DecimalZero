e13dc32e74d4f364aac4b576c4b216cd3eaafb38
AsadKirmani <32667125+AsadKirmani@users.noreply.github.com>
2024-12-03 17:18:23 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,33 +2,26 @@ import '../styles/globals.css';
 
 import axios from 'axios';
 import type { AppProps } from 'next/app';
-//import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	  //const [isHashValid, setIsHashValid] = useState(false);
-	    //const [username, setUsername] = useState<string | null>(null);
+const [isHashValid, setIsHashValid] = useState(false);
+const [username, setUsername] = useState<string | null>(null);
 
-	      /*useEffect(() => {
-		          if (typeof window !== 'undefined') {
-				        const initData = window.Telegram.WebApp.initData;
+useEffect(() => {
+	if (typeof window !== 'undefined') {
+		const initData = window.Telegram.WebApp.initData;
 
-					      axios
-					              .post('/api/validate-hash', { hash: initData })
-						              .then((response) => {
-								                setIsHashValid(response.status === 200);
-										          if (response.status === 200) {
-												              setUsername(window.Telegram.WebApp.initDataUnsafe?.user?.first_name || null);
-													                }
-															        })
-																        .catch(() => setIsHashValid(false));
-																	    }
-																	      }, []);
-
-																	        if (!isHashValid) {
-																			    return null;
-																			      }*/
-
-return <Component {...pageProps} />;
+		axios.post('/api/validate-hash', { hash: initData }).then((response) => {
+			setIsHashValid(response.status === 200);
+			if (response.status === 200) {
+				setUsername(window.Telegram.WebApp.initDataUnsafe?.user?.first_name || null);
+			}
+		}).catch(() => setIsHashValid(false));
+	}}, []);
+	if (!isHashValid) {
+		return null;
+	}
+	return <Component {...pageProps} />;
 }
-
 export default MyApp;
